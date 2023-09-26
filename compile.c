440f5dcfc16966dfca7e9904dc6fbe58d13fad9a
Asaadziad <asaadabuelhija@gmail.com>
2023-09-26 15:55:38 +0300

    Scanning tokens

--- compile.c
+++ compile.c
@@ -6,4 +6,17 @@
 
 void compile(const char* source){
     initScanner(source);
+    for(;;){
+        int line = -1;
+        Token token = scanToken();
+        if(token.line != line){
+            prinf("%4d", token.line);
+            line = token.line;
+        } else {
+            printf("    |");
+        }
+        printf("%2d '%.*s'", token.type,token.length,token.start);
+
+        if(token.type == TOKEN_EOF) break;
+    }
 }
