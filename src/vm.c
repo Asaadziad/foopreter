f9362927f1115b504c0658304226078a7e0960f1
Asaadziad <asaadabuelhija@gmail.com>
2023-10-03 21:29:21 +0300

    local variables

--- src/vm.c
+++ src/vm.c
@@ -138,6 +138,16 @@ static InterpretResult run(){
           printf("\n");
           break;
         }
+        case OP_GET_LOCAL: {
+          uint8_t slot = READ_BYTE();
+          push(vm.stack[slot]); 
+          break;
+        }
+        case OP_SET_LOCAL: {
+          uint8_t slot = READ_BYTE();
+          vm.stack[slot] = peek(0);
+          break;
+        }
         case OP_RETURN: return INTERPRET_OK; 
         case OP_POP: pop(); break;
 
